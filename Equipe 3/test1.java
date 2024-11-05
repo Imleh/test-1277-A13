f2b399d62c85f85aab7e1ce984b4ee5c9742d562
Imleh <helmiz1986@gmail.com>
2024-11-04 22:20:11 -0500

    Creer un fichier java

--- Equipe 3/test1.java
+++ Equipe 3/test1.java
@@ -1,5 +1,7 @@
 public class test1 {
     public static void main(String[] args) {
         system.out.println("bonjour.Ceci est une classe de test");
+        system.out.println("Fin code");
+
     }
 }
